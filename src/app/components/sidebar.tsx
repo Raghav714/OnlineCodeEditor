2fb1ba7516c85c7df787ff0de7395d19c3e41676
kai dubauskas <kaidubauskas@gmail.com>
2023-12-31 15:16:07 -0600

    implement draggable sidebar

--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -1,10 +1,17 @@
+
 import React, { useEffect, useContext, useState, useRef } from 'react';
+import { createPortal } from 'react-dom';
+import { DragDropContext, Droppable, Draggable, DropResult } from '@hello-pangea/dnd';
 import { AuthContext, LayoutContext, FileContext, ThemeContext, LanguageContext } from '../resources/contexts';
 import { ThemeBackgroundMap } from '../resources/themes';
 import { addCodeFile, getCodeFiles } from '../resources/pocketbase';
 import { LanguageMap, LanguageDefaultCode } from '../resources/languages';
 import '../styles/codeEditor.css';
 import SingleFile from './singleFile';
+
+// type File{
+
+// }
 
 const Sidebar: React.FC = () => {
     const inputRef = useRef<HTMLInputElement>(null);
@@ -116,58 +123,113 @@ const Sidebar: React.FC = () => {
     }
 
 
+    const onDragEnd = (result: DropResult) => {
+        if (!result.destination) {
+            return;
+        }
+        console.log("start: ", result.source.index);
+        console.log("destination: ", result.destination.index);
+        console.log(result);
+
+        // SetOrderIndex of  GetPythonFile(result.draggableId) to result.destination.index
+        // for idx in result.destination.index, files.length:
+        //     if files[idx] != result.draggableId:
+        //         IncremenetOrderIndex files[idx]
+
+        // IF NO ERRORS: 
+
+        const items = Array.from(files);
+        const [reorderedItem] = items.splice(result.source.index, 1);
+        items.splice(result.destination.index, 0, reorderedItem);
+
+        setFiles(items);
+    };
     return (
-        <div className={`${!isSidebarOpen ? 'hidden' : 'visible'} sidemirror-container dull`}
-            style={{
-                backgroundColor: backgroundColor,
-                color: textColor,
-                borderRight: `2px solid ${ThemeBackgroundMap[theme].gutterForeground}`
-            }}>
-            <div className="sidebar-header">
-                <h3 className='bright' style={{ borderBottom: `1px solid ${ThemeBackgroundMap[theme].gutterForeground}` }}>
-                    Files
-                </h3>
-                <button onClick={addFile} className='add-file-button' style={{
-                    color: textColor,
-                    backgroundColor: backgroundColor
-                }}>+</button>
-            </div>
-            {
-                files?.map((file: any) => {
-                    return <SingleFile
-                        key={file.id}
+        <DragDropContext onDragEnd={onDragEnd}>
+            <Droppable droppableId="filesDroppable">
+                {(provided) => (
+                    <div
+                        {...provided.droppableProps}
+                        ref={provided.innerRef}
+                        className={`${!isSidebarOpen ? 'hidden' : 'visible'} sidemirror-container dull`}
+                        style={{
+                            backgroundColor: backgroundColor,
+                            color: textColor,
+                            borderRight: `2px solid ${ThemeBackgroundMap[theme].gutterForeground}`
+                        }}
+                    >
+                        <div className="sidebar-header">
+                            <h3 className='bright' style={{ borderBottom: `1px solid ${ThemeBackgroundMap[theme].gutterForeground}` }}>
+                                Files
+                            </h3>
+                            <button onClick={addFile} className='add-file-button' style={{
+                                color: textColor,
+                                backgroundColor: backgroundColor
+                            }}>+</button>
+                        </div>
+                        {
+                            files?.map((file: any, index: number) => (
+                                <Draggable
+                                    key={file.id}
+                                    draggableId={`${file.id}`}
+                                    index={index}
+                                >
+                                    {(provided) => {
+                                        if (provided.draggableProps.style && "top" in provided.draggableProps.style)
+                                            provided.draggableProps.style.top -= 50;
+                                        return (
+                                            <div
+                                                ref={provided.innerRef}
+                                                {...provided.draggableProps}
+                                                {...provided.dragHandleProps}
+                                            >
 
-                        //Change So this uses context to get this maybe??
-                        id={file.id}
-                        title={file.title}
-                        lastUpdated={file.updated}
-                        dateCreated={file.created}
-                        language={file.language}
+                                                <SingleFile
+                                                    key={file.id}
 
-                        code={file.code}
-                        onClick={() => handleClickTitle(file.title, file.language)}
-                        handleDeleteFile={handleDeleteFile}
-                        handleRenameFile={handleRenameFile}
-                    />
-                })
-            }
-            {showInput && (
-                <input
-                    type="text"
-                    value={title}
-                    ref={inputRef}
-                    placeholder='Enter Filename'
-                    style={{
-                        color: textColor,
-                    }}
-                    className="sidebar-input-form"
-                    onChange={(e) => setTitle(e.target.value)}
-                    onKeyDown={handleSubmitFile}
-                    autoFocus
-                />
-            )}
-        </div>
+                                                    //Change So this uses context to get this maybe??
+                                                    id={file.id}
+                                                    title={file.title}
+                                                    lastUpdated={file.updated}
+                                                    dateCreated={file.created}
+                                                    language={file.language}
+
+                                                    code={file.code}
+                                                    onClick={() => handleClickTitle(file.title, file.language)}
+                                                    handleDeleteFile={handleDeleteFile}
+                                                    handleRenameFile={handleRenameFile}
+                                                />
+                                            </div>
+                                        )
+                                    }}
+                                </Draggable>
+                            ))
+                        }
+                        {showInput && (
+                            <input
+                                type="text"
+                                value={title}
+                                ref={inputRef}
+                                placeholder='Enter Filename'
+                                style={{
+                                    color: textColor,
+                                }}
+                                className="sidebar-input-form"
+                                onChange={(e) => setTitle(e.target.value)}
+                                onKeyDown={handleSubmitFile}
+                                autoFocus
+                            />
+                        )}
+                        {provided.placeholder}
+                    </div>
+                )}
+            </Droppable>
+        </DragDropContext>
     )
 }
+
+
+
+
 
 export default Sidebar;
